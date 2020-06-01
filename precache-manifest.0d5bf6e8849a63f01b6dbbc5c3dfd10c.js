580b982e85775fe6ce45fb2df8a16f132a6863e3
Satyaprakash verma <59075705+blue-run@users.noreply.github.com>
2020-06-01 16:08:41 +0530

    Updates

---
+++ precache-manifest.0d5bf6e8849a63f01b6dbbc5c3dfd10c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a12ed396f1c17de538138bf34bf7fc27",
+    "url": "/Robo-Contacts/index.html"
+  },
+  {
+    "revision": "dd745b5fd3377b560d66",
+    "url": "/Robo-Contacts/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "fc15b4ec4a5eaad53cdd",
+    "url": "/Robo-Contacts/static/css/main.4def7c54.chunk.css"
+  },
+  {
+    "revision": "dd745b5fd3377b560d66",
+    "url": "/Robo-Contacts/static/js/2.03f2c672.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Robo-Contacts/static/js/2.03f2c672.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fc15b4ec4a5eaad53cdd",
+    "url": "/Robo-Contacts/static/js/main.73da8aad.chunk.js"
+  },
+  {
+    "revision": "1acf853a499eaffa22bb",
+    "url": "/Robo-Contacts/static/js/runtime-main.b0d639de.js"
+  }
+]);
