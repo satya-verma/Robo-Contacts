58acd632bffe6a96ecbd433efe9ead19e47fa473
Satyaprakash verma <59075705+blue-run@users.noreply.github.com>
2020-06-02 13:38:28 +0530

    App.js changed

--- src/containers/App.js
+++ src/containers/App.js
@@ -16,7 +16,7 @@ class App extends React.Component {
     componentDidMount() {
         fetch('https://jsonplaceholder.typicode.com/users')
             .then(response => response.json())
-            .then(users => { this.setState({ robots: json }) });
+            .then(users => { this.setState({ robots: users }) });
     }
 
     onSearchChange = (event) => {
