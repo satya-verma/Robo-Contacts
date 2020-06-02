c6ac51ffdbff7112a5c4015136af11d8519a1619
Satyaprakash verma <59075705+blue-run@users.noreply.github.com>
2020-06-02 15:14:25 +0530

    Revert "fetch url changes"
    This reverts commit f08af0124b0651bcb81e4c0355dbd4d52e4e70fb.

--- src/containers/App.js
+++ src/containers/App.js
@@ -15,7 +15,7 @@ class App extends React.Component {
     }
 
     componentDidMount() {
-        fetch('http://jsonplaceholder.typicode.com/users')
+        fetch('https://jsonplaceholder.typicode.com/users')
             .then(response => response.json())
             .then(users => { this.setState({ robots: users }) });
     }
