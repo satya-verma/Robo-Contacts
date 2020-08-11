2934a4d3e8303e1c0742544fd18f9bfcb5088b4a
Satyaprakash verma <59075705+blue-run@users.noreply.github.com>
2020-08-11 12:03:00 +0530

    image files added

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -4,6 +4,7 @@ const SearchBox = ({ searchChange }) => {
     return (
         <div>
             <input
+                aria-label="search robots"
                 type="text"
                 placeholder="Search"
                 className="tc pa3 bg-near-black yellow mb3"
